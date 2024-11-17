5dc6ad28304b0b73e4385b21f9d30ca27798a7e4
Agsha Athalla Nurkareem <agshaathalla@gmail.com>
2024-11-18 03:42:52 +0700

    env configuration

--- src/database.js
+++ src/database.js
@@ -1,9 +1,13 @@
 const mysql = require('mysql2');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '123456',
-    database: 'daun_sehat'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 db.connect((err) => {
